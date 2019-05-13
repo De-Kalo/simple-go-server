77b7a21a73ec22a496ba329972874feb6097c654
Noam Shabat <noam.shabat@worthy.com>
2019-05-13 19:59:03 +0300

    adding home path

--- main.go
+++ main.go
@@ -13,10 +13,22 @@ import (
 func main() {
 	var router = mux.NewRouter()
 	router.HandleFunc("/healthcheck", HealthCheck).Methods("GET")
+	router.HandleFunc("/", Home).Methods("GET")
 
 	port := os.Getenv("PORT")
 	fmt.Println("Running server on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	type Result struct {
+		Status int
+		Msg    string
+	}
+	w.Header().Set("Content-Type", "application/json")
+	res := Result{0, "Honey, I'm home!"}
+	fmt.Println(res)
+	json.NewEncoder(w).Encode(res)
 }
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
